1df69fc83d94823b5b9ab5573d98a9f4fe68b8fa
Rahul Tiwari <rahultwr0005@gmail.com>
2021-11-07 15:07:29 +0530

    Update partition_equal_subset_sum.cpp

--- Leetcode/DP/partition_equal_subset_sum.cpp
+++ Leetcode/DP/partition_equal_subset_sum.cpp
@@ -66,3 +66,30 @@ public:
         return checkIsSumExists(nums, sum, n, dp);
     }
 };
+
+// more optimization:-
+class Solution {
+public:
+    
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+
+		int sum = 0;
+		for (int i = 0; i < n; i++)
+			sum += nums[i];
+
+		if (sum % 2 != 0) return false;
+
+		int target = sum/2;
+		vector<bool>dp(target+1, false);
+
+		dp[0] = true;
+		for (int i = 0; i < n; i++) {
+			for (int j = target; j >= nums[i]; j--) {
+				dp[j] = dp[j] | dp[j - nums[i]];
+			}
+		}
+
+		return dp[target];
+    }
+};
