d49de2324715eddc62f90a7229fa45d8a3b5cebb
Rahul Tiwari <rahultwr0005@gmail.com>
2021-12-17 01:30:39 +0530

    Update spiral_matrix.cpp

--- Leetcode/Array/spiral_matrix.cpp
+++ Leetcode/Array/spiral_matrix.cpp
@@ -50,3 +50,56 @@ public:
         return ans;
     }
 };
+
+// efficient approach...
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = m ? matrix[0].size() : 0;
+        
+        vector<int> ans;
+        if(m == 0 || n == 0) return ans;
+
+        int min_r = 0;
+        int min_c = 0;
+        
+        int max_r = m - 1;
+        int max_c = n - 1;
+        
+        int cnt = 1;
+        int total_elem = m * n;
+        
+        while(cnt <= total_elem) {
+            // top...
+            for(int i = min_r, j = min_c; j <= max_c && cnt <= total_elem; ++j) {
+                ans.push_back(matrix[i][j]);
+                cnt++;
+            }
+            min_r++;
+            
+            // right..
+            for(int i = min_r, j = max_c; i <= max_r && cnt <= total_elem; ++i) {
+                ans.push_back(matrix[i][j]);
+                cnt++;
+            }
+            max_c--;
+            
+            // bottom...
+            for(int i = max_r, j = max_c; j >= min_c && cnt <= total_elem; --j) {
+                ans.push_back(matrix[i][j]);
+                cnt++;
+            }
+            max_r--;
+            
+            // left...
+            for(int i = max_r, j = min_c; i >= min_r && cnt <= total_elem; --i) {
+                ans.push_back(matrix[i][j]);
+                cnt++;
+            }
+            min_c++;
+        }
+        
+        return ans;
+    }
+};
