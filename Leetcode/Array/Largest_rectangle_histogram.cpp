7e82ad9cb3094c9003dd776131efa231d8dfe0ef
Rahul Tiwari <rahultwr0005@gmail.com>
2021-12-18 15:24:35 +0530

    Update Largest_rectangle_histogram.cpp

--- Leetcode/Array/Largest_rectangle_histogram.cpp
+++ Leetcode/Array/Largest_rectangle_histogram.cpp
@@ -48,3 +48,40 @@ public:
         return max_area;
     }
 };
+
+// More space optimized solution...
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        
+        stack<int> st;
+        int max_area = 0;
+        
+        for(int i = 0; i < n; ++i) {
+            int curr = heights[i];
+            
+            while(!st.empty() && heights[st.top()] >= curr) {
+                int height = heights[st.top()];
+                st.pop();
+                int width = st.empty() ? i : i - st.top() - 1;
+                
+                int curr_area = height * width;
+                max_area = max(max_area, curr_area);
+            }
+            
+            st.push(i);
+        }
+        
+        while(!st.empty()) {
+            int height = heights[st.top()];
+            st.pop();
+            int width = st.empty() ? n : n - st.top() - 1;
+            
+            int curr_area = height * width;
+            max_area = max(max_area, curr_area);
+        }
+        
+        return max_area;
+    }
+};
