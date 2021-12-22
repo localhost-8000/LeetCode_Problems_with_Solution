28269e24e5c3996672ca435b8b746fd28624c0d9
Rahul Tiwari <rahultwr0005@gmail.com>
2021-12-22 19:56:27 +0530

    Create find_minimum_in_rotated_sorted_array.cpp

---
+++ Leetcode/Binary_Search/find_minimum_in_rotated_sorted_array.cpp
@@ -0,0 +1,30 @@
+// Problem link:-
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+        
+        if(n == 1 || nums[0] < nums[n-1]) return nums[0];
+        
+        int left = 0, right = n - 1;
+        int ans = -1;
+        
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            int curr = nums[mid];
+            if(curr > nums[mid+1]) {
+                ans = nums[mid + 1];
+                break;
+            }
+            
+            if(curr >= nums[left]) {
+                left = mid + 1;
+            } else if(curr <= nums[right]) {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
