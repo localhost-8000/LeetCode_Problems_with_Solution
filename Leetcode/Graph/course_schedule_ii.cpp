1fbaf5135781a3d1aca4ee16829ca43b1ef6a01e
Rahul Tiwari <rahultwr0005@gmail.com>
2021-12-23 11:33:03 +0530

    Create course_schedule_ii.cpp

---
+++ Leetcode/Graph/course_schedule_ii.cpp
@@ -0,0 +1,57 @@
+// Problem link:- https://leetcode.com/problems/course-schedule-ii
+
+
+class Solution {
+public:
+    bool dfs(vector<int> adj[], int s, bool visited[], bool recursionStack[], stack<int>& st) {
+        visited[s] = true;
+        recursionStack[s] = true;
+        
+        for(const int& v : adj[s]) {
+            if(!visited[v] && !dfs(adj, v, visited, recursionStack, st)) {
+                return false;
+            }
+            else if(recursionStack[v] == true) {
+                return false;
+            }
+        }
+        st.push(s);
+        recursionStack[s] = false;
+        
+        return true;
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = prerequisites.size();
+        vector<int> adj[numCourses];
+        
+        // create adjacency list...
+        for(const auto &arr : prerequisites) {
+            int u = arr[1];
+            int v = arr[0];
+            adj[u].push_back(v);
+        }
+        
+        bool visited[numCourses];
+        memset(visited, false, sizeof(visited));
+        bool recursionStack[numCourses];
+        memset(recursionStack, false, sizeof(recursionStack));
+        
+        vector<int> ans;
+        stack<int> st;
+        
+        for(int i=0; i<numCourses; ++i) {
+            if(!visited[i]) {
+                bool result = dfs(adj, i, visited, recursionStack, st);
+                
+                if(!result) return ans;
+            }
+        }
+        
+        while(!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        
+        return ans;
+    }
+};
