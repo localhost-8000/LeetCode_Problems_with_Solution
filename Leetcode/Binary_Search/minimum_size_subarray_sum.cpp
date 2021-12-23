c48b0c482252c9ec2058c06cfee4c682f526c84a
Rahul Tiwari <rahultwr0005@gmail.com>
2021-12-23 22:22:24 +0530

    Update minimum_size_subarray_sum.cpp

--- Leetcode/Binary_Search/minimum_size_subarray_sum.cpp
+++ Leetcode/Binary_Search/minimum_size_subarray_sum.cpp
@@ -1,6 +1,6 @@
 // Problem link:- https://leetcode.com/problems/minimum-size-subarray-sum
 
-
+// Binary search approach..O(NlogN)
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
@@ -19,6 +19,34 @@ public:
             auto itr = lower_bound(pre_sum.begin(), pre_sum.end(), search_num);
             if(itr != pre_sum.end()) 
                 ans = min(ans, (int)(itr - (pre_sum.begin() + i - 1)));
+        }
+        
+        if(ans == INT_MAX) return 0;
+        return ans;
+    }
+};
+
+
+// two pointer approach..O(N)
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+        
+        if(n == 0) return 0;
+        
+        int ans = INT_MAX;
+        
+        int left = 0;
+        int sum = 0;
+        
+        for(int i=0; i<n; ++i) {
+            sum += nums[i];
+            while(sum >= target) {
+                ans = min(ans, i + 1 - left);
+                sum -= nums[left];
+                left++;
+            }
         }
         
         if(ans == INT_MAX) return 0;
