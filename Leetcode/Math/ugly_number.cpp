359fd617968aab16a9402a93344d87479b9ad1c6
Rahul Tiwari <rahultwr0005@gmail.com>
2022-01-26 23:37:08 +0530

    Create ugly_number.cpp

---
+++ Leetcode/Math/ugly_number.cpp
@@ -0,0 +1,16 @@
+// Problem link:- https://leetcode.com/problems/ugly-number/
+
+class Solution {
+public:    
+    bool isUgly(int n) {
+        if(n < 1) return false;
+        
+        while(n % 2 == 0) n /= 2;
+        while(n % 3 == 0) n /= 3;
+        while(n % 5 == 0) n /= 5;
+        
+        if(n == 1) return true;
+        
+        return false;
+    }
+};
