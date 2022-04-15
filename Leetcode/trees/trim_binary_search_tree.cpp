1b871851855cea0d7691d9c9c3d544ada7d14b7f
Rahul Tiwari <rahultwr0005@gmail.com>
2022-04-15 11:13:47 +0530

    Create trim_binary_search_tree.cpp

---
+++ Leetcode/trees/trim_binary_search_tree.cpp
@@ -0,0 +1,20 @@
+// Problem link:- https://leetcode.com/problems/trim-a-binary-search-tree
+// Difficulty level:- Medium
+
+class Solution {
+public:
+    
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if(root == nullptr) return nullptr;
+        
+        int val = root->val;
+        TreeNode* new_node;
+        if(val < low) return trimBST(root->right, low, high);
+        else if(val > high) return trimBST(root->left, low, high);
+        
+        root->left = trimBST(root->left, low, high);
+        root->right = trimBST(root->right, low, high);
+        
+        return root;
+    }
+};
