6c7c8573409d8c9bde4b5e1a1eba52222932e8e4
Rahul Tiwari <rahultwr0005@gmail.com>
2022-05-30 23:37:28 +0530

    Update non_overlapping_interval.cpp

--- Leetcode/Greedy/non_overlapping_interval.cpp
+++ Leetcode/Greedy/non_overlapping_interval.cpp
@@ -28,3 +28,30 @@ public:
         return cnt;
     }
 };
+
+// greedy but based on ending time..
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b) -> bool {
+            return a[1] < b[1];
+        });
+        
+        int n = intervals.size();
+        int cnt = 0;
+        int prev_end = intervals[0][1];
+        
+        for(int i=1; i<n; ++i) {
+            int curr_start = intervals[i][0];
+            int curr_end = intervals[i][1];
+            
+            if(curr_start < prev_end) {
+                cnt++;
+            } else {
+                prev_end = curr_end;
+            }
+        }
+        
+        return cnt;
+    }
+};
