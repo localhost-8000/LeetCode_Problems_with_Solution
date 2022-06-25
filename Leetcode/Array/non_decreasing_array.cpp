60fb7073cb9290ef9e86476503427887ed917dcf
Rahul Tiwari <rahultwr0005@gmail.com>
2022-06-25 10:14:18 +0530

    Update non_decreasing_array.cpp

--- Leetcode/Array/non_decreasing_array.cpp
+++ Leetcode/Array/non_decreasing_array.cpp
@@ -1,6 +1,17 @@
 // Problem link:- https://leetcode.com/problems/non-decreasing-array/
 // Difficulty:- Medium
 
+
+// For i=0 to i<n-1, whenever we encounter the first invalid element i.e. arr[i] > arr[i+1], we have two choice
+// 1. decrease arr[i]
+// 2. increase arr[i+1]
+
+// To decrease the current element it must be substituted with x such that arr[i-1] <= x <= arr[i+1] ..... for i >= 0,
+// so calculate max(arr[i-1], arr[i+1]) and if it is not less or equal to arr[i+1] then we can't decrease the current element.
+
+// So we have the increase the next element i.e. arr[i+1] and the optimal case would be just make arr[i+1] = arr[i];
+// Flag variable is used to check that operation is performed at most once.
+    
 // O(N)..
 class Solution {
 public:
